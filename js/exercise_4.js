b3cb230c2110c2104c483821155b4ec3091a6247
Sam Baldwin <sambaldwin05@users.noreply.github.com>
2016-05-02 15:23:44 -0400

    FIrst stab at mapulating.

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -36,6 +36,6 @@ featureLayer.on('ready', function(){
 
 featureLayer.on('ready', function(){
 	this.eachLayer(function(layer){
-    	layer.bindPopup('Welcome to ' + layer.features.properties.name);
+    	layer.bindPopup('Welcome to ' + layer.feature.properties.name);
     });
 });
